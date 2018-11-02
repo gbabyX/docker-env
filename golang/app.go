81fd678a3f1029e75e67381b6439988bf7667fd5
xiongliuhua <xiongliuhua@360.net>
2018-11-02 15:55:54 +0800

    add redis conf && update app.go

--- golang/app.go
+++ golang/app.go
@@ -3,6 +3,11 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
-	router := gin.Default()
-	http.ListenAndServe(":80", router)
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() 
 }
